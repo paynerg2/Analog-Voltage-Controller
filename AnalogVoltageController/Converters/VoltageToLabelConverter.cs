0b12e6137d87df7445af88d5262c86ed521ee1e9
Rodney Payne <paynerg2@gmail.com>
2018-10-29 19:21:55 -0400

    UI Cleanup

---
+++ AnalogVoltageController/Converters/VoltageToLabelConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AnalogVoltageController.Converters
+{
+    public class VoltageToLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{value} V";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value;
+        }
+    }
+}
